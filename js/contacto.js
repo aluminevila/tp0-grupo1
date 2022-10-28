d6234e458e534924fac563860c1a62b044947c5f
Aluminé Vila <aluminevila@gmail.com>
2022-10-28 01:51:47 -0300

    formulario arreglado

--- js/contacto.js
+++ js/contacto.js
@@ -1,34 +1,58 @@
-/* --- validación del formulario ---*/
-function validarFormulario(evento) {
-    evento.preventDefault();
+function validarFormulario() {
     var nombre = document.getElementById('nombre').value;
-    if(nombre.length == 0) {
-        alert('Debe escribirse un nombre');
-        return;
-    }
-    var correo = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i.test(document.getElementById('correo').value);
-    if (correo == false) {
-        alert('La dirección de correo no es válida')
-        return
-    }
-    var telefono = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(document.getElementById('telefono').value);
-    if (telefono == false) {
-        alert('El teléfono no es válido')
-        return
-    }
+    var correo = document.getElementById('correo').value;
+    var telefono = document.getElementById('telefono').value;
     var mensaje = document.getElementById('mensaje').value;
-    if (mensaje.length == 0) {
-        alert('Debe escribirse un mensaje');
-        return;
-    }
-    this.submit();
+    
+    return validarNombre(nombre) && validarCorreo(correo) && validarTelefono(telefono) && validarMensaje(mensaje)
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("form").addEventListener('submit', validarFormulario); 
-});
+function validarNombre(nombre) {
+    if(nombre.length == 0) {
+        alert('Debe escribirse un nombre');
+        return false
+    }
+    return true
+}
 
-/*--- Formspree ---*/
+function validarCorreo(correo) {
+    var expresionRegular = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (!expresionRegular.test(correo)) {
+        alert('La dirección de correo no es válida')
+        return false
+    }
+    return true
+}
+
+function validarTelefono(telefono) {
+    var expresionRegular = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/
+    if (!expresionRegular.test(telefono)) {
+        alert('El teléfono no es válido')
+        return false
+    }
+    return true
+}
+
+function validarMensaje(mensaje) {
+    if(mensaje.length == 0) {
+        alert('Debe escribirse un mensaje');
+        return false
+    }
+    return true
+}
+
+
+
+
+async function enviarFormulario(event) {
+    if (validarFormulario(event)) {
+        await handleSubmit(event);
+    }
+}
+
+
+
+
 
 const $form = document.querySelector('#form')
 
@@ -36,18 +60,22 @@ $form.addEventListener('submit', handleSubmit)
 
 async function handleSubmit(event) {
     event.preventDefault()
-    var status = document.getElementById("my-form-status");
-    const form = new FormData(this);
-    const response = await fetch(this.action, {
+    if (validarFormulario()) {
+        var status = document.getElementById("my-form-status");
+        const form = new FormData(this);
+        const response = await fetch(this.action, {
         method: this.method,
         body: form,
         headers: {
             'Accept': 'application/json'
         }
-    })
-    if (response.ok) {
-        status.innerHTML = "Gracias por contactarnos!";
-        this.reset()
-        
+        })
+        if (response.ok) {
+            status.innerHTML = "Gracias por contactarnos!";
+            this.reset()
+            
+        }
     }
+    
 }
+
